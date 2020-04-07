4ca57bae2906ee6c6f76a619f303e0c7bd652466
giroberto <umbertobarros@gmail.com>
2020-04-07 00:18:51 -0300

    Added Js interations to searchbox

--- resources/views/livewire/search-dropdown.blade.php
+++ resources/views/livewire/search-dropdown.blade.php
@@ -1,7 +1,18 @@
-<div class="relative">
+<div class="relative" x-data="{ isOpen: true }" @click.away="{isOpen = false}">
     <input type="text" wire:model.debounce.500ms="search"
         class="bg-gray-800 rounded-full w-64 text-sm px-4 py-1 pl-8 focus:outline-none focus:shadow-outline mt-3 md:mt-0"
-        placeholder="Search">
+        placeholder="Search"
+        x-ref="search"
+        @keydown.window="
+        if(event.keyCode === 191){
+            event.preventDefault();
+            $refs.search.focus();
+        }
+        "
+        @focus="isOpen=true"
+        @keydown="isOpen=true"
+        @keydown.escape.window="isOpen=false"
+        @keydown.shift.tab="isOpen=false">
     <div class="absolute top-0">
         <svg class="fill-current w-4 text-gray-500 mt-2 ml-2" viewBox="0 0 24 24">
             <path class="heroicon-ui"
@@ -12,13 +23,14 @@
     <div wire:loading class="spinner top-0 right-0 mr-4 mt-4"></div>
 
     @if(strlen($search)>= 2)
-    <div class="absolute bg-gray-800 text-sm w-64 rounded mt-3">
+    <div class="z-50 absolute bg-gray-800 text-sm w-64 rounded mt-3" x-show.transition.opacity="isOpen">
         @if ($searchResults->count() > 0)
         <ul>
             @foreach ($searchResults as $result)
             <li class="border-b border-gray-700">
                 <a href="{{route('movies.show', $result['id'])}}"
-                    class="block hover:bg-gray-700 flex items-center px-3 py-3">
+                    class="block hover:bg-gray-700 flex items-center px-3 py-3" @if ($loop->last)
+                    @keydown.tab="isOpen=false" @endif>
                     @if($result['poster_path'])
                     <img src="{{ config('services.tmdb.image_url') . '/w92' . $result['poster_path'] }}" class="w-8"
                         alt='poster' />
